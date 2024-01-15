c43d8c9b18721749b1c9cb4b566666d8ca2338d6
rbozek <rbozek@gmail.com>
2024-01-15 14:16:18 -0500

    create models dir & flight.js, set up, define, export flight model

---
+++ models/flight.js
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose'
+
+// optional shortcut to the mongoose.Schema class
+const Schema = mongoose.Schema
+
+const flightSchema = new Schema({
+  airline: {
+    type: String,
+    enum: ['American', 'Southwest', 'United'],
+    // required: true
+  },  
+  airport: {
+    type: String,
+    default: 'DEN',
+    enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'],
+    // required: true
+  },
+  flightNo: {
+    type: Number,
+    required: true,
+    min: 11,
+    max: 9998
+  },
+  departs: {
+    type: Date,
+    default: function() {
+      // return (new Date().getFullYear() + 1)
+      // console.log((new Date().getFullYear() + 1));
+      const oneYearFromNow = new Date();
+      oneYearFromNow.setFullYear(oneYearFromNow.getFullYear() + 1);
+      // must return Date object, not just value
+      return oneYearFromNow;
+    },
+  },
+},{ 
+  timestamps: true
+})
+
+const Flight = mongoose.model('Flight', flightSchema)
+
+export {
+  Flight
+}
